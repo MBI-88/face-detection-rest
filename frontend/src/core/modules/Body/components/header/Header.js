f5287adf270e4c1ec911ec7219251d2bbc6e3cf7
MBI <ingmbi8807@gmail.com>
2022-12-08 07:48:49 -0300

    develop header body

--- frontend/src/core/modules/Body/components/header/Header.js
+++ frontend/src/core/modules/Body/components/header/Header.js
@@ -4,40 +4,78 @@ import { motion } from 'framer-motion';
 
 const Header = () => {
 
+    const container = {
+        hidden: { opacity: 0 },
+        show: {
+            opacity: 1,
+            transition: {
+                delayChildren: 1
+            }
+        }
+    }
+
+    const item1 = {
+        hidden: {
+            opacity: 0,
+            y: -20
+        },
+        show: {
+            opacity: 1,
+            y: 20,
+            transition: { duration: 1 }
+        }
+    }
+    const item2 = {
+        hidden: {
+            opacity: 0,
+            x: -100,
+        },
+        show: {
+            opacity: 1,
+            x: 15,
+            transition: { duration: 2 }
+        }
+    }
+    const item3 = {
+        hidden: { opacity: 0 },
+        show: {
+            opacity: 1,
+            transition: { duration: 6 }
+        }
+    }
 
     return (
         <article className='background-header mb-5 shadow'>
             <div className=' d-flex flex-row align-items-center'>
-                <div className='container ps-4'>
-                    <img src='/assets/recuento/header.png' className=' img-fluid ' alt='...' loading='lazy' />
-                </div>
-
+                <img src='/assets/recuento/header.png' className=' img-fluid w-25' alt='...' loading='lazy' />
                 <div className='image-2'>
-                    <motion.ul>
-                        <motion.li>
-                            <motion.h1 className='text-center fs-1 pt-4'
-                            animate={{ color: ['#40e612', '#7a19db9e', '#d31a1ab0', '#2551e2fe'], animationTimingFunction: 'ease' }}
-                            transition={{ duration: 10, repeat: Infinity, delay: 1 }}
-                            >
+                    <motion.ul variants={container} initial="hidden" animate="show" className='text-center'>
+                        <motion.li
+                            className=' nav-item text-center p-2' variants={item1}>
+                            <motion.h1 className='fs-1'
+                                animate={{ color: ['#40e612', '#7a19db9e', '#d31a1ab0', '#2551e2fe'], animationTimingFunction: 'ease' }}
+                                transition={{ duration: 10, repeat: Infinity, delay: 1 }}>
                                 Artifitial
                             </motion.h1>
                         </motion.li>
 
-                        <motion.li>
-                        <motion.h1 className='text-center fs-1 pt-1'
-                            animate={{ color: ['#40e612', '#7a19db9e', '#d31a1ab0', '#2551e2fe'], animationTimingFunction: 'ease' }}
-                            transition={{ duration: 10, repeat: Infinity, delay: 1 }}
-                            >
+                        <motion.li
+                            className='nav-item text-center' variants={item2}>
+                            <motion.h1 className='fs-1'
+                                animate={{ color: ['#40e612', '#7a19db9e', '#d31a1ab0', '#2551e2fe'], animationTimingFunction: 'ease' }}
+                                transition={{ duration: 10, repeat: Infinity, delay: 1 }}>
                                 Intelligent
                             </motion.h1>
                         </motion.li>
 
-                        <motion.li>
-                            <motion.section>
-
+                        <motion.li variants={item3} className="nav-item text-center pt-5">
+                            <motion.section className=' pt-5'>
+                                <p className='text-header fs-4'>
+                                    
+                                </p>
                             </motion.section>
                         </motion.li>
-                        
+
                     </motion.ul>
 
                     <img src='/assets/recuento/header2.jpg' className=' img-fluid' alt='...' />
